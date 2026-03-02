9dccc2c584fbc5413a9400a05b5237dd07abd924
OzoakRH <kanekineki50@gamil.com>
2026-03-02 10:07:26 +0700

    fixP02

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('test')
-  getTest() {
+  getTest(): any[] {
     return this.userService.test();
   }
 }
