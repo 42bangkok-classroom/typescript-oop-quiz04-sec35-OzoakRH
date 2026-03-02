4057e5fc133a5e8239ec03add75ce4e4bc144d74
OzoakRH <kanekineki50@gamil.com>
2026-03-02 09:55:24 +0700

    Update P02

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor (private readonly userService: UserService) {}
+    
+    @Get('test')
+    getTest() {
+        return this.userService.test()
+    }
+}
