305375f7a2cc1d5d2c06cbfefde71735bbe03db4
OzoakRH <kanekineki50@gmail.com>
2026-03-25 04:45:36 +0700

    Practice P03

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { IUser } from './user.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get()
+  findAll(): IUser[] {
+    return this.userService.findAll();
+  }
+  
   @Get('test')
   getTest(): string[] {
     return this.userService.test();
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './user.interface';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class UserService {
+  findAll(): IUser[] {
+    const filePath = path.resolve(process.cwd(), 'data/users.json');
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const users: IUser[] = JSON.parse(fileContent);
+    return users;
+  }
   test(): string[] {
     return [];
   }
