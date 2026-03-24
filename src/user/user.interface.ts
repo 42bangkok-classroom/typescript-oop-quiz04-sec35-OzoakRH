f72c2467d55e2682d79adcbc3f0418217f728ce9
OzoakRH <kanekineki50@gmail.com>
2026-03-25 05:27:01 +0700

    Pratice P04

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,8 +1,7 @@
 export interface IUser {
-  id: string;
-  firstName: string;
-  lastName;
-  string;
-  email: string;
-  username: string;
+    id: string;
+    firstName: string;
+    lastName; string;
+    email: string;
+    username: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { IUser } from './user.interface';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,10 +8,30 @@ export class UserService {
   test(): string[] {
     return [];
   }
+
   findAll(): IUser[] {
     const filePath = path.resolve(process.cwd(), 'data/users.json');
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const users: IUser[] = JSON.parse(fileContent) as IUser[];
     return users;
+  }
+
+  findOne(id: string, fields?: string[]): Partial<IUser> | IUser {
+    const users = this.findAll();
+    const user = users.find((u) => u.id === id);
+    
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (!fields || fields.length === 0) {
+      return user;
+    }
+    const filteredUser: Record<string, unknown> = {};
+    fields.forEach((field) => {
+      if (field in user) {
+        filteredUser[field] = (user as unknown as Record<string, unknown>)[field];
+      }
+    });
+    return filteredUser as Partial<IUser>;
   }
 }
